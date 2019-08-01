15cab30c7694dcde3df2e81e19c8e24dce235299
YEJINLONGxy <1067530182@qq.com>
2019-08-01 15:50:43 +0800

    控制流 if-else

---
+++ TrueValueDetection.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+#检测真值的优雅方式是这样：
+if x:
+	pass
+
+#不要像下面这样做：
+if x == True:
+	pass
---
+++ number100.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+number = int(input("Enter a number: "))
+if number < 100:
+	print("The number si less than 100")
+else:
+	print("The number is greater than 100")
